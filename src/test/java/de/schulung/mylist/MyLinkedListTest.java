c4d1bee7b68ec5c9505ead3c9f2101a95b604a04
Michael Thomas <michit.berlin@googlemail.com>
2026-06-10 11:39:57 +0200

    added mylinkedlist tests

--- src/test/java/de/schulung/mylist/MyLinkedListTest.java
+++ src/test/java/de/schulung/mylist/MyLinkedListTest.java
@@ -1,4 +1,63 @@
 package de.schulung.mylist;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MyLinkedListTest {
+
+    private MyLinkedList<String> list;
+
+    @BeforeEach
+    public void setUp() {
+        list = new MyLinkedList<>();
+    }
+
+    @Test
+    void newList() {
+        assertEquals(0, list.size());
+        assertNull(list.get(0));
+    }
+
+    @Test
+    void addFirstElement() {
+        list.add("John");
+
+        assertEquals(1, list.size());
+        assertEquals("John", list.get(0));
+    }
+
+    @Test
+    void removeLastAvailableElement() {
+        list.add("John");
+        list.remove(0);
+
+        assertEquals(0, list.size());
+        assertNull(list.get(0));
+    }
+
+    @Test
+    void removeFirstElement() {
+        list.add("John");
+        list.add("Paul");
+
+        list.remove(0);
+
+        assertEquals(1, list.size());
+        assertEquals("Paul", list.get(0));
+    }
+
+    @Test
+    void removeMiddleElement() {
+
+        list.add("John");
+        list.add("Paul");
+        list.add("Carl");
+
+        list.remove(1);
+
+        assertEquals(2, list.size());
+        assertEquals("John", list.get(0));
+        assertEquals("Carl", list.get(1));
+    }
 }
